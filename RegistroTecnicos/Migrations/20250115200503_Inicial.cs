1b5b21eeed8467381d4e59a0ad6fa8a27f883112
ESMAILINM <esmailinmartinez05@gmail.com>
2025-01-18 02:32:16 -0400

    Creacion del Contexto, de la migracios y de la clase Tecnicos.

---
+++ RegistroTecnicos/Migrations/20250115200503_Inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tecnicos",
+                columns: table => new
+                {
+                    TecnicosId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombres = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SueldoHora = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tecnicos", x => x.TecnicosId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tecnicos");
+        }
+    }
+}
---
+++ RegistroTecnicos/Models/Tecnicos.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class Tecnicos
+    {
+        [Key]
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Nombres { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        public decimal SueldoHora { get; set; }
+   
+    }
+}
