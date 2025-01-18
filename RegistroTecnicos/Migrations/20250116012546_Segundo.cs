ff654c7f43c9baffccfd3e4f38b31a99f520dfed
ESMAILINM <esmailinmartinez05@gmail.com>
2025-01-18 02:38:20 -0400

    Segunda migracion para editar TecnicosId a TecnicoId como atributo de la base de datos.

---
+++ RegistroTecnicos/Migrations/20250116012546_Segundo.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicos.Migrations
+{
+    /// <inheritdoc />
+    public partial class Segundo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TecnicosId",
+                table: "Tecnicos",
+                newName: "TecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TecnicoId",
+                table: "Tecnicos",
+                newName: "TecnicosId");
+        }
+    }
+}
