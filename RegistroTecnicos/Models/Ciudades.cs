893af27d1a50c23514c87f942cb869864fde6bf2
ESMAILINM <esmailinmartinez05@gmail.com>
2025-01-27 23:25:57 -0400

    Crea el Modelo Ciudades

---
+++ RegistroTecnicos/Models/Ciudades.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnicos.Models
+{
+    public class Ciudades
+    {
+        [Key]
+        public int CiudadId{ get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
