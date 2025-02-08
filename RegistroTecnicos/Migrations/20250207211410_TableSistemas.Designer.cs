bfcb4684add00b9c28b7d4ac42c8196a01619d49
ESMAILINM <esmailinmartinez05@gmail.com>
2025-02-07 22:17:48 -0400

    Crea las migraciones y actualiza la base de datos

--- RegistroTecnicos/Migrations/20250207211410_TableSistemas.Designer.cs
+++ RegistroTecnicos/Migrations/20250207211410_TableSistemas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RegistroTecnicos.DAL;
 
@@ -11,9 +12,11 @@ using RegistroTecnicos.DAL;
 namespace RegistroTecnicos.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20250207211410_TableSistemas")]
+    partial class TableSistemas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
